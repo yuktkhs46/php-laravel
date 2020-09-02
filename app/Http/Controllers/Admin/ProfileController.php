4bd764c14b5ff1437888361bef7aecae516b2858
EC2 Default User <ec2-user@ip-172-31-47-149.us-east-2.compute.internal>
2020-09-02 22:56:02 +0000

    PHP/Laravel 17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -38,6 +38,21 @@ public function create(Request $request)
       return redirect('admin/profile/create');
   }  
 
+//index
+ public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのプロフィールを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
+
 
 public function edit(Request $request)
 {
