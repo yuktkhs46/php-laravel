d11b2ec3d56edb278c0ee74dbfc7d313ccbc2fde
EC2 Default User <ec2-user@ip-172-31-47-149.us-east-2.compute.internal>
2020-09-05 04:38:14 +0000

    PHP/Laravelコース - Herokuへのデプロイ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <h1>Hello!</h1>
+        <h1>Hello!Would!</h1>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
